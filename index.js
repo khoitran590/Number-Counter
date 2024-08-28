ea434ed50fd9222cb2fc401d9148c81325d63a14
Peter Tran <khoitran590@gmail.com>
2024-08-28 11:41:27 -0700

    Complete basic functionalities

---
+++ index.js
@@ -0,0 +1,19 @@
+const decreaseBtn = document.getElementById("btn-decrease");
+const increaseBtn = document.getElementById("btn-increase");
+const resetBtn = document.getElementById("btn-reset");
+const countLabel = document.getElementById("countLabel");
+
+let count = 0;
+
+increaseBtn.onclick = function(){
+    count++;
+    countLabel.textContent = count;
+}
+decreaseBtn.onclick = function(){
+    count--;
+    countLabel.textContent = count;
+}
+resetBtn.onclick = function(){
+    count = 0;
+    countLabel.textContent = count;
+}
